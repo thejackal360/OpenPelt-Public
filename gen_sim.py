d77aad263a33cb0ab8d86ce94d143a28bfabe269
Roman <rparise@besseltechnologies.com>
2021-12-23 17:13:01 -0800

    Added temperature coefficient of resistivity to the model

--- gen_sim.py
+++ gen_sim.py
@@ -22,20 +22,22 @@ class tec_pms:
                        m_metal, \
                        c_metal, \
                        rs, \
-                       ambient_t):
-        self.l_tec = l_tec
-        self.k_tec = k_tec
-        self.a_tec = a_tec
-        self.m_tec = m_tec
-        self.c_tec = c_tec
-        self.alpha = alpha
-        self.l_metal = l_metal
-        self.k_metal = k_metal
-        self.a_metal = a_metal
-        self.m_metal = m_metal
-        self.c_metal = c_metal
-        self.rs = rs
+                       ambient_t, \
+                       tc_tec):
+        self.l_tec     = l_tec
+        self.k_tec     = k_tec
+        self.a_tec     = a_tec
+        self.m_tec     = m_tec
+        self.c_tec     = c_tec
+        self.alpha     = alpha
+        self.l_metal   = l_metal
+        self.k_metal   = k_metal
+        self.a_metal   = a_metal
+        self.m_metal   = m_metal
+        self.c_metal   = c_metal
+        self.rs        = rs
         self.ambient_t = ambient_t
+        self.tc_tec    = tc_tec
 
 ### Generate wav file ###
 # data -> a numpy array of data
@@ -62,7 +64,8 @@ def gen_pms(pms_obj):
                              m_metal   = pms_obj.m_metal, \
                              c_metal   = pms_obj.c_metal, \
                              rs        = pms_obj.rs, \
-                             ambient_t = pms_obj.ambient_t))
+                             ambient_t = pms_obj.ambient_t, \
+                             tc_tec    = pms_obj.tc_tec))
 
 ### Call ngspice ###
 def call_ngspice():
@@ -95,7 +98,8 @@ if __name__ == "__main__":
                   m_metal = 2710.0 * 40.0e-3 * 40.0e-3 * 6.35e-3, # [kg], mass of plate \
                   c_metal = 0.89e3, # [J/kg*K], aluminum specific heat cap \
                   rs = 4.00, # [Ohm], TEC electrical resistance \
-                  ambient_t = 27.00) # [C], ambient temp of ngspice sims
+                  ambient_t = 27.00, # [C], ambient temp of ngspice sims \
+                  tc_tec = 0.004) # [Ohm/K], temp coefficient of electrical resistivity
     t = np.linspace(10.00e-3, 1.00, 1000)
     y = len(t) * [4000.00]
     # plt.plot(t, y)
--- output/params.mod
+++ output/params.mod
@@ -1,32 +1,34 @@
 *** TEC Parameters ***
 * Thickness of TEC. Units: ?
-.PARAM L_TEC = 0.2
+.PARAM L_TEC = 0.0036
 * Thermal conductivity of TEC. Units: ?
-.PARAM k_TEC = 0.5
+.PARAM k_TEC = 1.2
 * Area of TEC. Units: ?
-.PARAM A_TEC = 0.2
+.PARAM A_TEC = 0.0016
 * Mass of TEC. Units: ?
-.PARAM m_TEC = 0.1
+.PARAM m_TEC = 0.025
 * Specific heat capacity of TEC. Units: ?
-.PARAM c_TEC = 0.9
+.PARAM c_TEC = 323.0
+* Electrical resistivity temperature coefficient. Units: ?
+.PARAM tc_TEC = 0.004
 * Seebeck coefficient. Units: ?
 * TODO: TEMP DEPENDENT
-.PARAM alpha = 0.1
+.PARAM alpha = 0.053
 
 *** Metal Heating Plate Parameters ***
 * Thickness of metal. Units: ?
-.PARAM L_METAL = 0.1
+.PARAM L_METAL = 0.00635
 * Thermal conductivity of metal. Units: ?
-.PARAM k_METAL = 0.2
+.PARAM k_METAL = 150.0
 * Area of metal. Units: ?
-.PARAM A_METAL = 0.65
+.PARAM A_METAL = 0.0016
 * Mass of metal plate
-.PARAM m_METAL = 0.1
+.PARAM m_METAL = 0.027533600000000002
 * Specific heat capacity of metal plate
-.PARAM c_METAL = 0.3
+.PARAM c_METAL = 890.0
 
 *** Electrical Circuit Parameters ***
-.PARAM Rs = 40.0
+.PARAM Rs = 4.0
 
 *** Ambient Temperature ***
 .PARAM AMBIENT_TEMP = 27.0
