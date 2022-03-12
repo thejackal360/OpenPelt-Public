a925bcfb27109618844b40f5ba603703d88b138a
Roman Parise <rparise@besseltechnologies.com>
2022-03-11 17:28:16 -0800

    Remove spaces to maintain style conventions

--- OpenPelt/OpenPelt.py
+++ OpenPelt/OpenPelt.py
@@ -244,8 +244,8 @@ class tec_plant(Circuit):
         self.plate_select = plate_select
         self.ncs = tec_lib(self.controller_f,
                            send_data=True,
-                           plate_select = self.plate_select,
-                           steady_state_cycles = steady_state_cycles)
+                           plate_select=self.plate_select,
+                           steady_state_cycles=steady_state_cycles)
         if sig_type == Signal.VOLTAGE:
             self.V(INPUT_SRC, '11', self.gnd, 'dc 0 external')
         else:
