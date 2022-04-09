0a746c7db79df7faacc5b52c4064a301885dc32a
gdetor <gdetor@gmail.com>
2022-04-09 14:19:13 -0700

    README file issues fixed

--- build_ngspice.sh
+++ build_ngspice.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
 
-cd thejackal360-ngspice/ ; ./autogen.sh ; ./configure --with-ngshared --prefix=`pwd` ; make ; make install ; cd -
+cd thejackal360-ngspice/ || exit;
+./autogen.sh;
+./configure "--with-ngshared --prefix=$(pwd)";
+make;
+make install;
+cd - || exit
